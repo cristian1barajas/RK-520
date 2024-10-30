10a4756fac269696064ace2c6ca3e6dd20f7f259
cristian1barajas <cristian1barajas@hotmail.com>
2024-10-30 12:56:12 -0500

    Mejora en la implementación de la lectura de los datos del sensor

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 // Global variables
 Rika520 sensor;
 unsigned long previousMillis = 0;
-const long interval = 1000;
+const long interval = 5000;
 
 void setup() {
     Serial.begin(9600);
@@ -15,13 +15,13 @@ void loop() {
     if (currentMillis - previousMillis >= interval) {
         previousMillis = currentMillis;
         sensor.writeRika();
-        
-        SensorData data = sensor.readSense();
-        if(data.isValid) {
-            Serial.print("Temperature: "); Serial.print(data.temperature, 1); Serial.println(" C");
-            Serial.print("Moisture: "); Serial.print(data.moisture, 1); Serial.println(" %");
-            Serial.print("EC: "); Serial.print(data.ec, 3); Serial.println(" mS/cm");
-            Serial.println("-----");
-        }
+    }
+
+    SensorData data = sensor.readSense();
+    if(data.isValid) {
+        Serial.print("Temperature: "); Serial.print(data.temperature, 1); Serial.println(" C");
+        Serial.print("Moisture: "); Serial.print(data.moisture, 1); Serial.println(" %");
+        Serial.print("EC: "); Serial.print(data.ec, 3); Serial.println(" mS/cm");
+        Serial.println("-----");
     }
 }
